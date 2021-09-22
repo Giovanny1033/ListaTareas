f27989abf4b648e6bb15ac041ad417053004d21a
Giovanny1033 <gzamora18@uan.edu.co>
2021-09-22 01:04:29 -0500

    ClickListener

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -7,10 +7,8 @@ import android.widget.*
 class MainActivity : AppCompatActivity() {
 
     val itemlist = arrayListOf<String>("Tarea1, tarea2, tarea3, tarea4")
-    var adapter = ArrayAdapter<String>(
-        this,
-        android.R.layout.simple_list_item_multiple_choice, itemlist
-    )
+    var adapter: ArrayAdapter<String> ?= null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +18,20 @@ class MainActivity : AppCompatActivity() {
         adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemlist)
         val addListView = findViewById<ListView>(R.id.listView)
         addListView.adapter = adapter
-        addListView.setOnClickListener {
+        val editText = findViewById<EditText>(R.id.editText)
+        val add = findViewById<Button>(R.id.add)
 
+        add.setOnClickListener{
+            itemlist.add(editText.text.toString())
+            adapter?.notifyDataSetChanged()
+        }
+
+        
+
+
+
+
+        addListView.setOnClickListener {
 
             itemlist.add(editText.text.toString())
             val listView = null
